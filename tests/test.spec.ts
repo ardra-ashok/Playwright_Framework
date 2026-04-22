4cbd57e7a93c9a845e0d6a0aeb8f9f5815c8e2c3
ardra_ashok <ardraasok@yahoo.in>
2026-04-23 01:10:06 +1000

    added fixtures for login and  dashboard

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,10 +1,10 @@
 
 import { test } from '../utils/fixtures';
+import loginData from '../support/users.json';
 
 test.describe('Secure Area Tests', () => {
-
- test('should access dashboard immediately', async ({ authenticatedPage, dashboardPage }) => {
-     await dashboardPage.verifyIsLoaded('John Doe','admin');
- });
-
+    test('should access dashboard immediately', async ({ authenticatedPage, dashboardPage }) => {
+     const user = loginData.validUsers[0]; 
+     await dashboardPage.verifyIsLoaded(user.display_name,user.type);
+    });
 });
