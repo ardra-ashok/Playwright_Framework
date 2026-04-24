7911264a7bca5b65b5dbb15ed1348796ddcd180b
ardra_ashok <ardraasok@yahoo.in>
2026-04-25 01:14:25 +1000

    added security check verificarion

--- pages/DashboardPage.ts
+++ pages/DashboardPage.ts
@@ -8,14 +8,21 @@ export class DashboardPage extends BasePage {
  private readonly DASHBOARD_ADMIN = ROUTES.ADMIN_DASHBOARD;
  private readonly accountName;
  private readonly dashboardMessage;
+ private readonly pageTitle;
 
  constructor(page: Page) {
   super(page);
   this.accountName = page.locator('#menu')
   this.dashboardMessage = page.locator('h1[data-test="page-title"]')
+  this.pageTitle = page.locator('.ch-title.spacer-bottom') 
  }
  
- async verifyIsLoaded(name: string, type: string) {
+  async verifyIsLoaded(name: string, type: string) {
+   const title = await this.page.title();
+    if (title.includes('security') || await this.page.locator('text=Performing security verification').isVisible()) {
+      await this.page.waitForNavigation({ waitUntil: 'networkidle' });
+    }
+
   await expect(this.accountName, 'User should see the Account name on the dashboard').toHaveText(name)
   
   switch (true) {
